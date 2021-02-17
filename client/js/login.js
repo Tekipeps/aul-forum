926d786dfc6bae765f00a27bf6001a74d847037e
Tekena Solomon <tekena157@gmail.com>
2021-02-17 11:20:53 +0100

    Get login details from form

---
+++ client/js/login.js
@@ -0,0 +1,11 @@
+const loginForm = document.getElementById("login-form")
+
+const matric = loginForm.querySelector("input[name='matric-no']")
+const password = loginForm.querySelector("input[name='password']")
+
+
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const loginData = { username: matric.value, password: password.value }
+    console.log(loginData)
+})
