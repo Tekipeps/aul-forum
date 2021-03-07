5b5e3de61c4383d36bc150bbfdea1162d4e6cd62
Tekena Solomon <tekena157@gmail.com>
2021-03-07 12:42:05 +0100

    Secure post routes

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -11,7 +11,7 @@ class Auth {
             if (!user) return res.status(400).json("user not available, please register")
             const isCorrectPassword = await bcrypt.compare(password, user.passwordHash)
             if (!isCorrectPassword) return res.status(401).json({ err: "incorrect username or password" })
-            const pubUser = { username: user.username, email: user.email, role: user.role }
+            const pubUser = { username: user.username, email: user.email, role: user.role, id: user.id }
             const token = jwt.sign(pubUser, JWT_SECRET)
             res.json({ user: pubUser, token })
         } catch (error) {
@@ -34,7 +34,7 @@ class Auth {
             const passwordHash = await bcrypt.hash(password, 12)
             const user = { username, email, matric, gender, role: "USER", passwordHash }
             const savedUser = await prisma.user.create({ data: user })
-            const pubUser = { username: savedUser.username, email: savedUser.email, role: savedUser.role }
+            const pubUser = { username: savedUser.username, email: savedUser.email, role: savedUser.role, id: savedUser.id }
             const token = jwt.sign(pubUser, JWT_SECRET)
             res.json({ user: pubUser, token })
         } catch (error) {
