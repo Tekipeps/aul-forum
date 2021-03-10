05bb5bc4c703e6748fe57e3c28c82cbe4e81f5e0
Tekena Solomon <tekena157@gmail.com>
2021-03-11 00:48:23 +0100

    Development (#22)
    * Setup validation, Finished auth validation

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,4 +1,5 @@
 const { prisma, JWT_SECRET } = require('../utils/config');
+const { role } = require('../utils/globals');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
@@ -6,42 +7,52 @@ class Auth {
     async login(req, res, next) {
         try {
             const { username, password } = req.body;
-            if (!username || !password) return res.status(400).json({ err: 'username and password required' });
-            const user = await prisma.user.findUnique({ where: { username } });
-            if (!user) return res.status(400).json('user not available, please register');
+            const user = await prisma.user.findUnique({
+                where: { username },
+                select: {
+                    username: true,
+                    email: true,
+                    role: true,
+                    id: true,
+                    passwordHash: true
+                }
+            });
+            if (!user)
+                return res.status(400).json({
+                    err: 'User not available, please register'
+                });
             const isCorrectPassword = await bcrypt.compare(password, user.passwordHash);
-            if (!isCorrectPassword) return res.status(401).json({ err: 'incorrect username or password' });
-            const pubUser = { username: user.username, email: user.email, role: user.role, id: user.id };
-            const token = jwt.sign(pubUser, JWT_SECRET);
-            res.json({ user: pubUser, token });
+            if (!isCorrectPassword)
+                return res.status(401).json({
+                    err: 'Incorrect username or password'
+                });
+            delete user.passwordHash;
+            const token = jwt.sign(user, JWT_SECRET);
+            res.json({ user, token });
         } catch (error) {
             next(error);
         }
     }
     async register(req, res, next) {
         try {
-            const { username, email, matric, gender, password, confirmPass } = req.body;
-            if (!username || !email || !matric || !gender) {
-                return res.status(400).json({ err: 'username, email, matric, password, gender, is required' });
-            }
-            if (String(password).length < 8) {
-                return res.status(400).json({ err: 'password too short, 8 or more characters' });
-            }
-            if (password !== confirmPass) {
-                return res.status(400).json({ err: 'passwords not the same' });
-            }
+            const { username, password, email, matric, gender } = req.body;
 
             const passwordHash = await bcrypt.hash(password, 12);
-            const user = { username, email, matric, gender, role: 'USER', passwordHash };
-            const savedUser = await prisma.user.create({ data: user });
-            const pubUser = {
-                username: savedUser.username,
-                email: savedUser.email,
-                role: savedUser.role,
-                id: savedUser.id
-            };
-            const token = jwt.sign(pubUser, JWT_SECRET);
-            res.json({ user: pubUser, token });
+            const savedUser = await prisma.user.create({
+                data: { username, email, matric, gender, role: role.USER, passwordHash },
+                select: {
+                    username: true,
+                    email: true,
+                    role: true,
+                    id: true
+                }
+            });
+            const token = jwt.sign(savedUser, JWT_SECRET);
+
+            res.json({
+                user: savedUser,
+                token
+            });
         } catch (error) {
             next(error);
         }
