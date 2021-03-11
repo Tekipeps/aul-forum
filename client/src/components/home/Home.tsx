311cdd8f01b683fcfe32357c253603bbdb02fa83
bids416007 <33462386+bids416007@users.noreply.github.com>
2021-03-11 09:55:04 +0000

    fixed readme length #23

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -3,7 +3,6 @@ import PostsSection from './PostsSection';
 //import MembersSection from './MembersSection';
 import styled from 'styled-components';
 
-<<<<<<< HEAD
 const PostsSectionWrapper = styled.div`
     position: relative;
     width: 100%;
@@ -16,11 +15,6 @@ const HomeWrapper = styled.div`
 export const Home: FC = (): ReactElement => (
     <HomeWrapper>
         <PostsSectionWrapper>
-=======
-export const Home: FC = (): ReactElement => (
-    <div>
-        <div id='home-content'>
->>>>>>> 05bb5bc4c703e6748fe57e3c28c82cbe4e81f5e0
             <PostsSection />
         </PostsSectionWrapper>
     </HomeWrapper>
--- client/src/components/team/TeamMember.tsx
+++ client/src/components/team/TeamMember.tsx
@@ -1,10 +1,6 @@
 import React, { ReactElement, FC } from 'react';
 import githubLogo from '../../assets/github-logo.svg';
-<<<<<<< HEAD
 import styles from './Team.module.scss';
-=======
-
->>>>>>> 05bb5bc4c703e6748fe57e3c28c82cbe4e81f5e0
 interface Member {
     name: string;
     avatarURL: string;
@@ -14,17 +10,10 @@ interface Member {
 }
 
 const TeamMember: FC<Member> = ({ name, avatarURL, bio, role, githubURL }): ReactElement => (
-<<<<<<< HEAD
     <div className={styles.member}>
         <img className={styles.avatar} src={avatarURL} alt={name} />
         <p className={styles.role}>{role}</p>
         <h3 className={styles.name}>{name}</h3>
-=======
-    <div className='member'>
-        <img className='avatar' src={avatarURL} alt={name} />
-        <p className='role'>{role}</p>
-        <h3 className='name'>{name}</h3>
->>>>>>> 05bb5bc4c703e6748fe57e3c28c82cbe4e81f5e0
 
         <p className={styles.bio}>{bio}</p>
         <a href={githubURL}>
