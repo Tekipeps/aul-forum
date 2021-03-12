a080ebf5545a56005dac0faba120a3a933c9cf9e
bids416007 <33462386+bids416007@users.noreply.github.com>
2021-03-12 18:20:34 +0000

    Integrated Nunito font

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -1,6 +1,17 @@
 import { ReactElement, FC } from 'react';
 import PostsSection from './PostsSection';
+//import MembersSection from './MembersSection';
+//import styled from 'styled-components';
+/*
+const div = styled.div`
+    position: relative;
+    width: 100%;
+    float: left;
+`;
 
+const div = styled.div`
+    display: flex;
+`;*/
 export const Home: FC = (): ReactElement => (
     <div>
         <div>
