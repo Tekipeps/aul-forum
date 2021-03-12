a06f5c5d0d0881bb67baeebc737d4f9b1509ddea
bids416007 <33462386+bids416007@users.noreply.github.com>
2021-03-12 17:57:42 +0000

    removed styled components

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -1,21 +1,10 @@
 import { ReactElement, FC } from 'react';
 import PostsSection from './PostsSection';
-//import MembersSection from './MembersSection';
-import styled from 'styled-components';
 
-const PostsSectionWrapper = styled.div`
-    position: relative;
-    width: 100%;
-    float: left;
-`;
-
-const HomeWrapper = styled.div`
-    display: flex;
-`;
 export const Home: FC = (): ReactElement => (
-    <HomeWrapper>
-        <PostsSectionWrapper>
+    <div>
+        <div>
             <PostsSection />
-        </PostsSectionWrapper>
-    </HomeWrapper>
+        </div>
+    </div>
 );
