7ca2a0e1b71e91946ee960d2b3f31dc4730ed3a0
Bidemi <33462386+BidemiEnoch@users.noreply.github.com>
2021-03-12 18:32:31 +0000

    Nunito will now be used for main contents, very neat font, also... INTERFACES over {a,b,c,d}:{a:type,b:type,c:type,d:type}, much cleaner (#43)
    * Integrated Nunito font
    
    * Fixed issues #36 and #42
    
    Co-authored-by: bids416007 <33462386+bids416007@users.noreply.github.com>

--- client/src/services/auth_service.ts
+++ client/src/services/auth_service.ts
@@ -2,7 +2,12 @@ import axios from 'axios';
 
 const baseUrl = '/api/auth';
 
-const login = async ({ username, password }: { username: string; password: string }) => {
+interface data{
+    username:string;
+    password:string;
+}
+
+const login = async ({ username, password }: data) => {
     const response = await axios.post(`${baseUrl}/login`, { username, password });
 
     return response.data
